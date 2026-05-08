5150a73e695c76a048872480631b76f8dd2ebcc1
kornixd <niuroad@gmail.com>
2026-05-08 22:18:56 +0300

    Create main.jsx

---
+++ src/src/main.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+import Home from './pages/Home.jsx'
+import WinGo from './pages/WinGo.jsx'
+import Wallet from './pages/Wallet.jsx'
+import Deposit from './pages/Deposit.jsx'
+import Withdraw from './pages/Withdraw.jsx'
+import Invite from './pages/Invite.jsx'
+import Profile from './pages/Profile.jsx'
+
+export const API = 'https://bezztrade-backend.onrender.com'
+
+function PrivateRoute({ children }) {
+  const token = localStorage.getItem('token')
+  return token ? children : <Navigate to="/login" />
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
+        <Route path="/wingo" element={<PrivateRoute><WinGo /></PrivateRoute>} />
+        <Route path="/wallet" element={<PrivateRoute><Wallet /></PrivateRoute>} />
+        <Route path="/deposit" element={<PrivateRoute><Deposit /></PrivateRoute>} />
+        <Route path="/withdraw" element={<PrivateRoute><Withdraw /></PrivateRoute>} />
+        <Route path="/invite" element={<PrivateRoute><Invite /></PrivateRoute>} />
+        <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
